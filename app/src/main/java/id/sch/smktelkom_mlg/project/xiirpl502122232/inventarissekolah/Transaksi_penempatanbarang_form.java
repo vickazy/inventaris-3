ab26ffaf313d6e32868e124c5b574cd6e27fdea6
NAFI <irfannafiyanto@gmail.com>
2016-11-19 21:04:29 +0700

    pembuatan layout form input

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl502122232/inventarissekolah/Transaksi_penempatanbarang_form.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl502122232.inventarissekolah;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Transaksi_penempatanbarang_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaksi_penempatanbarang_form);
+    }
+}
