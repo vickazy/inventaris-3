673a5e1a538d506b872ac7f85e45c2851038c13b
NAFI <irfannafiyanto@gmail.com>
2016-11-19 21:27:53 +0700

    perbaikan layout form input

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl502122232/inventarissekolah/Master_sumberpengadaanbarang.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl502122232/inventarissekolah/Master_sumberpengadaanbarang.java
@@ -1,7 +1,10 @@
 package id.sch.smktelkom_mlg.project.xiirpl502122232.inventarissekolah;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 
 public class Master_sumberpengadaanbarang extends Activity {
 
@@ -9,5 +12,14 @@ public class Master_sumberpengadaanbarang extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_sumberpengadaanbarang);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Master_sumberpengadaanbarang.this, Master_sumberpengadaanbarang_form.class);
+                startActivity(intent);
+            }
+        });
     }
 }
